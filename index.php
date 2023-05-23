cef875a96264c750c2b0c714e5ee93d39d6441ff
CarloBattista <carlitobatti@gmail.com>
2023-05-23 16:03:19 +0200

    card musiche in modo statico

--- index.php
+++ index.php
@@ -20,6 +20,71 @@
                 <img class="spotify_logo" src="./_assets/_resources/Spotify_Icon_RGB_Green.svg" alt="Spotify logo">
             </a>
         </div>
+        <!-- Main -->
+        <div class="main">
+            <div class="container_cards w-80">
+                <div class="card_music">
+                    <div class="image_music">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" alt="">
+                    </div>
+                    <div class="info_music">
+                        <h2 class="title_music">Somebody To Love</h2>
+                        <h3 class="artist_music">Freddie Mercury</h3>
+                        <p class="year_release">1976</p>
+                    </div>
+                </div>
+                <div class="card_music">
+                    <div class="image_music">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" alt="">
+                    </div>
+                    <div class="info_music">
+                        <h2 class="title_music">Somebody To Love</h2>
+                        <h3 class="artist_music">Freddie Mercury</h3>
+                        <p class="year_release">1976</p>
+                    </div>
+                </div>
+                <div class="card_music">
+                    <div class="image_music">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" alt="">
+                    </div>
+                    <div class="info_music">
+                        <h2 class="title_music">Somebody To Love</h2>
+                        <h3 class="artist_music">Freddie Mercury</h3>
+                        <p class="year_release">1976</p>
+                    </div>
+                </div>
+                <div class="card_music">
+                    <div class="image_music">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" alt="">
+                    </div>
+                    <div class="info_music">
+                        <h2 class="title_music">Somebody To Love</h2>
+                        <h3 class="artist_music">Freddie Mercury</h3>
+                        <p class="year_release">1976</p>
+                    </div>
+                </div>
+                <div class="card_music">
+                    <div class="image_music">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" alt="">
+                    </div>
+                    <div class="info_music">
+                        <h2 class="title_music">Somebody To Love</h2>
+                        <h3 class="artist_music">Freddie Mercury</h3>
+                        <p class="year_release">1976</p>
+                    </div>
+                </div>
+                <div class="card_music">
+                    <div class="image_music">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b" alt="">
+                    </div>
+                    <div class="info_music">
+                        <h2 class="title_music">Somebody To Love</h2>
+                        <h3 class="artist_music">Freddie Mercury</h3>
+                        <p class="year_release">1976</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Vue via CDN -->
