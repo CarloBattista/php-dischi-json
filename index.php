a6b107e00c4a29ce7df329272aba376e96ef7e53
CarloBattista <carlitobatti@gmail.com>
2023-05-23 15:41:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-dischi-json</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./_assets/css/main.css">
+</head>
+<body>
+    
+    <!-- Div di collegemaneto per Vue -->
+    <div id="app">
+        <h1>{{ message }}</h1>
+    </div>
+
+    <!-- Vue via CDN -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- Axios via CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- Link file Javascript -->
+    <script src="./_assets/js/main.js"></script>
+</body>
+</html>
